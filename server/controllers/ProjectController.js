934d76935038d3419d56e4659961ba68ac563da3
qlfzn <aqilfauzan09@gmail.com>
2025-03-04 03:48:17 +0800

    setup backend application with nodejs

---
+++ server/controllers/ProjectController.js
@@ -0,0 +1,12 @@
+/* 
+This controller handles the CRUD operations of projects published by clients 
+*/
+
+
+// Add project
+
+// Get projects list of a certain client
+
+// Update project status
+
+// Remove project
